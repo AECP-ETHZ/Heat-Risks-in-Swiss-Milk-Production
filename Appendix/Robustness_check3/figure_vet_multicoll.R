094a313bdc9c343cb63ee543b0ef96bf37c52e00
JanicBucheli <72181864+JanicBucheli@users.noreply.github.com>
2022-08-11 13:23:55 +0200

    Add files via upload

---
+++ Appendix/Robustness_check3/figure_vet_multicoll.R
@@ -0,0 +1,150 @@
+# ==================================================================================================================================
+# ==================================================================================================================================
+#
+# Modular code for the publication: Heat Risks in Swiss Milk production
+#
+# Citation: Bucheli, J., Uldry, M. and Finger, R. 2022. Heat Risks in Swiss Milk production. Journal of the Agricultural and Applied
+#           Economics Association.
+#
+# Robustness check 3: figure without precipitation for veterinary expenses
+#
+# ==================================================================================================================================
+# ==================================================================================================================================
+
+library(lmtest)
+library(dplyr)
+library(ggplot2)
+library(egg)
+
+# Firstly, run script 01
+
+plot_titles <- c("a) complete sample","b) plain sample", "c) hill sample", "d) mountain sample")
+load("Meteo/THI/THI_farm.RData")
+colnames(farm_THI_hourly)[1] <- "farm"
+load("APPENDIX/Multicollinearity/final_knots_vet.RData")
+load("APPENDIX/Multicollinearity/final_model_vet.RData")
+
+sub_panels_ID <- list(good_farms_panel,good_farms_panel_plain, good_farms_panel_hill, good_farms_panel_mountain)
+
+# ---------------------
+# Plot for each sample
+# ---------------------
+
+THI_range <- seq(ceiling(min(farm_THI_hourly$THI, na.rm=T)), floor(max(farm_THI_hourly$THI, na.rm=T)))
+list_absolut_plots <- list()
+list_centered_plots <- list()
+
+for (s in 1:length(plot_titles)){
+  
+  # Data preparation
+  temp_model <- list_final_model_vet[[s]]
+  temp_knot  <- list_final_knot_combination_vet[[s]]
+  
+  sub_dairy_farms_panel_final <- dairy_farms_panel_final[which(dairy_farms_panel_final$farm %in% sub_panels_ID[[s]] & dairy_farms_panel_final$sk_Tierarzt > 0),]
+  number_farms <- length(unique( sub_dairy_farms_panel_final$farm))
+  
+  sub_farm_THI_hourly <- farm_THI_hourly[which(farm_THI_hourly$farm %in% unique(sub_dairy_farms_panel_final$farm)),]
+  THI_min <- ceiling(min(sub_farm_THI_hourly$THI, na.rm=T))
+  THI_max <- floor(max(sub_farm_THI_hourly$THI, na.rm=T))
+  THI_mean <- mean(sub_farm_THI_hourly$THI, na.rm=T)
+  
+  # Calculate absolute effects
+  if(!is.na(temp_knot)){
+  effect_THI <- lspline(THI_range, knots = temp_knot) %*% coef(temp_model)[1:(length(temp_knot) + 1)]
+  effect_error_min <- effect_THI - ((lspline(THI_range, knots=temp_knot)) %*% (abs(coeftest(temp_model))[,2][1:(length(temp_knot) + 1)]*1.96))
+  effect_error_max <- effect_THI + ((lspline(THI_range, knots=temp_knot)) %*% (abs(coeftest(temp_model))[,2][1:(length(temp_knot) + 1)]*1.96))
+  summary_effects_THI <- as.data.frame(bind_cols(THI_range,effect_THI,effect_error_min,effect_error_max))
+  colnames(summary_effects_THI) <- c("THI","Effect","lower_band","upper_band")
+  } else {
+    effect_THI <- (THI_range) * coef(temp_model)[1]
+    effect_error_min <- effect_THI - (THI_range * (abs(coeftest(temp_model))[,2][1])*1.96)
+    effect_error_max <- effect_THI + (THI_range * (abs(coeftest(temp_model))[,2][1])*1.96)
+    summary_effects_THI <- as.data.frame(bind_cols(THI_range,effect_THI,effect_error_min,effect_error_max))
+    colnames(summary_effects_THI) <- c("THI","Effect","lower_band","upper_band")
+  }
+  
+  
+  calculated_effect <- summary_effects_THI[which(summary_effects_THI$THI >= THI_min & summary_effects_THI$THI <= THI_max),]
+  
+  list_absolut_plots[[s]] <- ggplot()+ggtitle(paste(plot_titles[s],"\n", "(",number_farms," farms",")",sep=""))+
+    theme(panel.grid.major = element_blank(),
+          plot.title = element_text(family="Times New Roman", size=12, colour = "grey25", hjust=0.5),
+          plot.subtitle = element_text(family="Times New Roman", size=12, colour = "grey25", hjust=0.5),
+          axis.title = element_text(family="Times New Roman", size=12, colour = "grey25"),
+          axis.text = element_text(family="Times New Roman", size=12),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black"),
+          legend.position="none")+
+    
+    xlab("THI") + ylab("log(veterinary expenses) [CHF]")+
+    scale_x_continuous(breaks = seq(-10,90, by=20), limits = c(min(THI_range),max(THI_range)))+
+    scale_y_continuous(breaks = c(-0.003,-0.002,-0.001,0,0.001,0.002,0.003), limits = c(-0.004,0.004))+
+    geom_hline(yintercept=c(-0.003,-0.002,-0.001,0,0.001,0.002,0.003), colour="gray", alpha=0.3)+
+    geom_hline(yintercept=c(0), colour="darkgray", size=1.05)+
+    geom_vline(xintercept=temp_knot, linetype="dotted", colour="darkgray", alpha=0.5, size=0.75)+
+    
+    # Calculated effect
+    geom_line(data=calculated_effect, aes(y=Effect, x=THI,), size=1, colour="brown1")+
+    geom_line(data=calculated_effect, aes(y=upper_band, x=THI,), size=0.5, colour="brown1", alpha=1)+
+    geom_line(data=calculated_effect, aes(y=lower_band, x=THI,), size=0.5, colour="brown1", alpha=1)+
+    geom_ribbon(data=calculated_effect,aes(ymin=lower_band, ymax=upper_band, x=THI), fill="brown1",alpha=0.1)+
+ 
+    
+  rm(calculated_effect, projected_effect_low, projected_effect_up)
+  
+  
+  # Calculate centered effects
+  if(!is.na(temp_knot)){
+  effect_THI_centered <- effect_THI - as.vector(lspline(THI_mean, knots = temp_knot)[1,] %*% coef(temp_model)[1:(length(temp_knot) + 1)])
+  effect_error_min_centered <- effect_error_min - as.vector(lspline(THI_mean, knots = temp_knot)[1,] %*% coef(temp_model)[1:(length(temp_knot)+1)])
+  effect_error_max_centered <- effect_error_max - as.vector(lspline(THI_mean, knots = temp_knot)[1,] %*% coef(temp_model)[1:(length(temp_knot)+1)])
+  } else {
+    effect_THI_centered <- effect_THI - (THI_mean * coef(temp_model)[1])
+    effect_error_min_centered <- effect_error_min - (THI_mean * coef(temp_model)[1])
+    effect_error_max_centered <- effect_error_max - (THI_mean * coef(temp_model)[1])
+  }
+  
+  summary_effects_THI_centered <- as.data.frame(bind_cols(THI_range,effect_THI_centered,effect_error_min_centered,effect_error_max_centered))
+  colnames(summary_effects_THI_centered) <- c("THI","Effect","lower_band","upper_band")
+  
+  
+  calculated_effect <- summary_effects_THI_centered[which(summary_effects_THI_centered$THI >= THI_min & summary_effects_THI_centered$THI <= THI_max),]
+  
+  THI_mean_df <- as.data.frame(matrix(NA,nrow=1, ncol=2))
+  colnames(THI_mean_df) <- c("THI", "Effect")
+  THI_mean_df[1,1] <- THI_mean
+  THI_mean_df[1,2] <- 0
+  
+  list_centered_plots[[s]] <- ggplot()+ggtitle(paste(plot_titles[s],"\n", "(",number_farms," farms",")",sep=""))+
+    theme(panel.grid.major = element_blank(),
+          plot.title = element_text(family="Times New Roman", size=9, colour = "grey25", hjust=0.5),
+          plot.subtitle = element_text(family="Times New Roman", size=7, colour = "grey25", hjust=0.5),
+          axis.title = element_text(family="Times New Roman", size=7, colour = "grey25"),
+          axis.text = element_text(family="Times New Roman", size=7),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black"),
+          legend.position="none")+
+    
+    xlab("THI") + ylab("log(veterinary expenses) [CHF]")+
+    scale_x_continuous(breaks = seq(40,90, by=10), limits = c(40,max(THI_range)))+
+    scale_y_continuous(breaks = c(-0.004,-0.002,0,0.002,0.004), limits = c(-0.0045,0.0045))+
+    geom_hline(yintercept=c(-0.004,-0.002,0,0.002,0.004), colour="gray", alpha=0.3)+
+    geom_hline(yintercept=c(0), colour="darkgray", size=0.5)+
+    {if (!is.na(temp_knot))geom_vline(xintercept=temp_knot, linetype="dotted", colour="darkgray", alpha=0.5, size=0.75)}+
+    
+    # Show the average THI and estimated effect
+    geom_point(data=THI_mean_df,aes(x=THI, y= Effect), colour="darkgray", alpha=1)+
+    
+    # Calculated effect
+    geom_line(data=calculated_effect, aes(y=Effect, x=THI,), size=0.5, colour="brown1")+
+    geom_line(data=calculated_effect, aes(y=upper_band, x=THI,), size=0.25, colour="brown1", alpha=1)+
+    geom_line(data=calculated_effect, aes(y=lower_band, x=THI,), size=0.25, colour="brown1", alpha=1)+
+    geom_ribbon(data=calculated_effect,aes(ymin=lower_band, ymax=upper_band, x=THI), fill="brown1",alpha=0.3)
+ 
+}
+
+plot_vet_centered <- ggarrange(list_centered_plots[[1]],list_centered_plots[[2]],list_centered_plots[[3]],list_centered_plots[[4]])
+ggsave(plot=plot_vet_centered,"APPENDIX/Multicollinearity/vet_main.pdf", width = 16, height = 16, unit="cm", device = cairo_pdf)
+ggsave(plot=plot_vet_centered,"APPENDIX/Multicollinearity/vet_main.png", width = 16, height = 9, unit="cm")
---
+++ Appendix/Robustness_check4/figure_feed_combined.R
@@ -0,0 +1,207 @@
+# ==================================================================================================================================
+# ==================================================================================================================================
+#
+# Modular code for the publication: Heat Risks in Swiss Milk production
+#
+# Citation: Bucheli, J., Uldry, M. and Finger, R. 2022. Heat Risks in Swiss Milk production. Journal of the Agricultural and Applied
+#           Economics Association.
+#
+# Robustness check 4: figure for feed purchases
+#
+# ==================================================================================================================================
+# ==================================================================================================================================
+
+library(lmtest)
+library(dplyr)
+library(ggplot2)
+library(egg)
+
+# Firstly, run script 01
+
+plot_titles <- c("a) complete sample","b) plain sample", "c) hill sample", "d) mountain sample")
+load("Meteo/Temperature/farm_T_daily.RData")
+colnames(farm_T_hourly)[1] <- "farm"
+colnames(farm_T_hourly)[5] <- "Temperature"
+
+# Load farms with high milk production
+load("APPENDIX/Milchleistung/final_knots_feed_highmilk.RData")
+load("APPENDIX/Milchleistung/final_model_feed_highmilk.RData")
+
+list_final_model_feed_up <- list_final_model_feed
+list_final_knot_combination_feed_up <- list_final_knot_combination_feed
+rm(list_final_model_feed, list_final_knot_combination_feed)
+
+# Correction (wrong knot location)
+list_final_knot_combination_feed_up[[3]] <- NA
+
+# Load farms with low milk production
+load("APPENDIX/Milchleistung/final_knots_feed__milkdown.RData")
+load("APPENDIX/Milchleistung/final_model_feed_milkdown.RData")
+
+list_final_model_feed_down <- list_final_model_feed
+list_final_knot_combination_feed_down <- list_final_knot_combination_feed
+
+# Correction (wrong knot location)
+list_final_knot_combination_feed_down[[2]] <- NA
+
+# Load these from APP_01_sample_geo_MilchleistungUp.R
+sub_panels_ID_up <- list(good_farms_panel,good_farms_panel_plain, good_farms_panel_hill, good_farms_panel_mountain)
+
+# Calculate again sub-panel from downs from APP_01_geo_
+sub_panels_ID_down <- list(good_farms_panel,good_farms_panel_plain, good_farms_panel_hill, good_farms_panel_mountain)
+
+# ---------------------
+# Plot for each sample
+# ---------------------
+
+Temperature_range <- seq(ceiling(min(farm_T_hourly$Temperature, na.rm=T)), floor(max(farm_T_hourly$Temperature, na.rm=T)),0.0025)
+list_absolut_plots <- list()
+list_centered_plots <- list()
+
+for (s in 1:length(plot_titles)){
+  
+  # Data preparation
+  temp_model_up <- list_final_model_feed_up[[s]]
+  temp_knot_up  <- list_final_knot_combination_feed_up[[s]]
+  
+  sub_dairy_farms_panel_final_up <- dairy_farms_panel_final[which(dairy_farms_panel_final$farm %in% sub_panels_ID_up[[s]] & dairy_farms_panel_final$sk_Futter_tot > 0),]
+  number_farms_up <- length(unique( sub_dairy_farms_panel_final_up$farm))
+  
+  sub_farm_T_hourly_up <- farm_T_hourly[which(farm_T_hourly$farm %in% unique(sub_dairy_farms_panel_final_up$farm)),]
+  T_min_up <- ceiling(min(sub_farm_T_hourly_up$Temperature, na.rm=T))
+  T_max_up <- floor(max(sub_farm_T_hourly_up$Temperature, na.rm=T))
+  T_mean_up <- mean(sub_farm_T_hourly_up$Temperature, na.rm=T)
+  
+  # Calculate absolute effects
+  if(!is.na(temp_knot_up)){
+  effect_T_up <- lspline(Temperature_range, knots = temp_knot_up) %*% coef(temp_model_up)[1:(length(temp_knot_up) + 1)]
+  effect_error_min_up <- effect_T_up - ((lspline(Temperature_range, knots=temp_knot_up)) %*% (abs(coeftest(temp_model_up))[,2][1:(length(temp_knot_up) + 1)]*1.96))
+  effect_error_max_up <- effect_T_up + ((lspline(Temperature_range, knots=temp_knot_up)) %*% (abs(coeftest(temp_model_up))[,2][1:(length(temp_knot_up) + 1)]*1.96))
+  summary_effects_T_up <- as.data.frame(bind_cols(Temperature_range,effect_T_up,effect_error_min_up,effect_error_max_up))
+  colnames(summary_effects_T_up) <- c("Temperature","Effect","lower_band","upper_band")
+  } else {
+    effect_T_up <- (Temperature_range) * coef(temp_model_up)[1]
+    effect_error_min_up <- effect_T_up - (Temperature_range * (abs(coeftest(temp_model_up))[,2][1])*1.96)
+    effect_error_max_up <- effect_T_up + (Temperature_range * (abs(coeftest(temp_model_up))[,2][1])*1.96)
+    summary_effects_T_up <- as.data.frame(bind_cols(Temperature_range,effect_T_up,effect_error_min_up,effect_error_max_up))
+    colnames(summary_effects_T_up) <- c("Temperature","Effect","lower_band","upper_band")
+  }
+  
+
+  temp_model_down <- list_final_model_feed_down[[s]]
+  temp_knot_down  <- list_final_knot_combination_feed_down[[s]]
+  
+  sub_dairy_farms_panel_final_down <- dairy_farms_panel_final[which(dairy_farms_panel_final$farm %in% sub_panels_ID_down[[s]] & dairy_farms_panel_final$sk_Futter_tot > 0),]
+  number_farms_down <- length(unique( sub_dairy_farms_panel_final_down$farm))
+  
+  sub_farm_T_hourly_down <- farm_T_hourly[which(farm_T_hourly$farm %in% unique(sub_dairy_farms_panel_final_down$farm)),]
+  T_min_down <- ceiling(min(sub_farm_T_hourly_down$Temperature, na.rm=T))
+  T_max_down <- floor(max(sub_farm_T_hourly_down$Temperature, na.rm=T))
+  T_mean_down <- mean(sub_farm_T_hourly_down$Temperature, na.rm=T)
+  
+  # Calculate absolute effects
+  if(!is.na(temp_knot_down) == T){
+    effect_T_down <- lspline(Temperature_range, knots = temp_knot_down) %*% coef(temp_model_down)[1:(length(temp_knot_down) + 1)]
+    effect_error_min_down <- effect_T_down - ((lspline(Temperature_range, knots=temp_knot_down)) %*% (abs(coeftest(temp_model_down))[,2][1:(length(temp_knot_down) + 1)]*1.96))
+    effect_error_max_down <- effect_T_down + ((lspline(Temperature_range, knots=temp_knot_down)) %*% (abs(coeftest(temp_model_down))[,2][1:(length(temp_knot_down) + 1)]*1.96))
+    summary_effects_T_down <- as.data.frame(bind_cols(Temperature_range,effect_T_down,effect_error_min_down,effect_error_max_down))
+    colnames(summary_effects_T_down) <- c("Temperature","Effect","lower_band","upper_band")
+  } else {
+    effect_T_down <- (Temperature_range) * coef(temp_model_down)[1]
+    effect_error_min_down <- effect_T_down - (Temperature_range * (abs(coeftest(temp_model_down))[,2][1])*1.96)
+    effect_error_max_down <- effect_T_down + (Temperature_range * (abs(coeftest(temp_model_down))[,2][1])*1.96)
+    summary_effects_T_down <- as.data.frame(bind_cols(Temperature_range,effect_T_down,effect_error_min_down,effect_error_max_down))
+    colnames(summary_effects_T_down) <- c("Temperature","Effect","lower_band","upper_band")
+  }
+  
+  
+  # Calculate centered effects
+  if(!is.na(temp_knot_up)==T){
+  effect_T_centered_up <- effect_T_up - as.vector(lspline(T_mean_up, knots = temp_knot_up)[1,] %*% coef(temp_model_up)[1:(length(temp_knot_up) + 1)])
+  effect_error_min_centered_up <- effect_error_min_up - as.vector(lspline(T_mean_up, knots = temp_knot_up)[1,] %*% coef(temp_model_up)[1:(length(temp_knot_up)+1)])
+  effect_error_max_centered_up <- effect_error_max_up - as.vector(lspline(T_mean_up, knots = temp_knot_up)[1,] %*% coef(temp_model_up)[1:(length(temp_knot_up)+1)])
+  } else {
+    effect_T_centered_up <- effect_T_up - (T_mean_up * coef(temp_model_up)[1])
+    effect_error_min_centered_up <- effect_error_min_up - (T_mean_up * coef(temp_model_up)[1])
+    effect_error_max_centered_up <- effect_error_max_up - (T_mean_up * coef(temp_model_up)[1])
+  }
+  
+  summary_effects_T_centered_up <- as.data.frame(bind_cols(Temperature_range,effect_T_centered_up,effect_error_min_centered_up,effect_error_max_centered_up))
+  colnames(summary_effects_T_centered_up) <- c("Temperature","Effect","lower_band","upper_band")
+  
+  
+  calculated_effect_up <- summary_effects_T_centered_up[which(summary_effects_T_centered_up$Temperature >= T_min_up & summary_effects_T_centered_up$Temperature <= T_max_up),]
+  
+  if(!is.na(temp_knot_down)==T){
+    effect_T_centered_down <- effect_T_down - as.vector(lspline(T_mean_down, knots = temp_knot_down)[1,] %*% coef(temp_model_down)[1:(length(temp_knot_down) + 1)])
+    effect_error_min_centered_down <- effect_error_min_down - as.vector(lspline(T_mean_down, knots = temp_knot_down)[1,] %*% coef(temp_model_down)[1:(length(temp_knot_down)+1)])
+    effect_error_max_centered_down <- effect_error_max_down - as.vector(lspline(T_mean_down, knots = temp_knot_down)[1,] %*% coef(temp_model_down)[1:(length(temp_knot_down)+1)])
+  } else {
+    effect_T_centered_down <- effect_T_down - (T_mean_down * coef(temp_model_down)[1])
+    effect_error_min_centered_down <- effect_error_min_down - (T_mean_down * coef(temp_model_down)[1])
+    effect_error_max_centered_down <- effect_error_max_down - (T_mean_down * coef(temp_model_down)[1])
+  }
+  
+  summary_effects_T_centered_down <- as.data.frame(bind_cols(Temperature_range,effect_T_centered_down,effect_error_min_centered_down,effect_error_max_centered_down))
+  colnames(summary_effects_T_centered_down) <- c("Temperature","Effect","lower_band","upper_band")
+  
+  
+  calculated_effect_down <- summary_effects_T_centered_down[which(summary_effects_T_centered_down$Temperature >= T_min_down & summary_effects_T_centered_down$Temperature <= T_max_down),]
+
+  T_mean_up_df <- as.data.frame(matrix(NA,nrow=1, ncol=2))
+  colnames(T_mean_up_df) <- c("Temperature", "Effect")
+  T_mean_up_df[1,1] <- T_mean_up
+  T_mean_up_df[1,2] <- 0
+  
+  T_mean_down_df <- as.data.frame(matrix(NA,nrow=1, ncol=2))
+  colnames(T_mean_down_df) <- c("Temperature", "Effect")
+  T_mean_down_df[1,1] <- T_mean_down
+  T_mean_down_df[1,2] <- 0
+  
+  
+  list_centered_plots[[s]] <- ggplot()+ggtitle(paste(plot_titles[s],sep=""))+
+    theme(panel.grid.major = element_blank(),
+          plot.title = element_text(family="Times New Roman", size=9, colour = "grey25", hjust=0.5),
+          plot.subtitle = element_text(family="Times New Roman", size=7, colour = "grey25", hjust=0.5),
+          axis.title = element_text(family="Times New Roman", size=7, colour = "grey25"),
+          axis.text = element_text(family="Times New Roman", size=7),
+          panel.grid.minor = element_blank(),
+          panel.background = element_blank(),
+          axis.line = element_line(colour = "black"),
+          legend.position="none")+
+    
+    xlab("Temperature") + ylab("log(feed purchases) [CHF]")+
+    scale_x_continuous(breaks = seq(5,40, by=10), limits = c(5,max(Temperature_range)))+
+    scale_y_continuous(breaks = c(-0.004,-0.002,0,0.002,0.004), limits = c(-0.0055,0.0065))+
+    geom_hline(yintercept=c(-0.004,-0.002,0,0.002,0.004), colour="gray", alpha=0.3)+
+    geom_hline(yintercept=c(0), colour="darkgray", size=0.5)+
+    {if (!is.na(temp_knot_down))geom_vline(xintercept=temp_knot_down, linetype="dotted", colour="darkgray", alpha=0.5, size=0.75)}+
+    {if (!is.na(temp_knot_up))geom_vline(xintercept=temp_knot_up, linetype="dotted", colour="darkgray", alpha=1.5, size=0.75)}+
+    
+    
+    geom_rect(aes(xmin=-5, xmax=max(Temperature_range), ymin=0.0041, ymax=0.0065), color="white",fill="white")+
+    
+    annotate("text", x = 15, y = 0.0060, label = paste("high milk yield subsample (",number_farms_up," farms)",sep=""), colour = "brown1",family="Times New Roman",size=2)+
+    annotate("text", x = 14.5, y = 0.0050, label = paste("low milk yield subsample (",number_farms_down," farms)",sep=""), colour = "cornflowerblue",family="Times New Roman",size=2)+
+    # Show the average THI and estimated effect
+    geom_point(data=T_mean_down_df,aes(x=Temperature, y= Effect), colour="darkgray", alpha=1)+
+    geom_point(data=T_mean_up_df, aes(x=Temperature, y= Effect), colour="gray24", alpha=4)+
+    
+    # Calculated effect (upper)
+    geom_line(data=calculated_effect_up, aes(y=Effect, x=Temperature,), size=0.5, colour="brown1")+
+    geom_line(data=calculated_effect_up, aes(y=upper_band, x=Temperature,), size=0.25, colour="brown1", alpha=1)+
+    geom_line(data=calculated_effect_up, aes(y=lower_band, x=Temperature,), size=0.25, colour="brown1", alpha=1)+
+    geom_ribbon(data=calculated_effect_up,aes(ymin=lower_band, ymax=upper_band, x=Temperature), fill="brown1",alpha=0.3)+
+    
+    # Calculated effect (lower)
+    geom_line(data=calculated_effect_down, aes(y=Effect, x=Temperature,), size=0.5, colour="cornflowerblue")+
+    geom_line(data=calculated_effect_down, aes(y=upper_band, x=Temperature), size=0.25, colour="cornflowerblue", alpha=1)+
+    geom_line(data=calculated_effect_down, aes(y=lower_band, x=Temperature), size=0.25, colour="cornflowerblue", alpha=1)+
+    geom_ribbon(data=calculated_effect_down,aes(ymin=lower_band, ymax=upper_band, x=Temperature), fill="cornflowerblue",alpha=0.3)
+}
+
+plot_feed_centered <- ggarrange(list_centered_plots[[1]],list_centered_plots[[2]],list_centered_plots[[3]],list_centered_plots[[4]])
+
+ggsave(plot=plot_feed_centered,"APPENDIX/Milchleistung/feed_samples.pdf", width = 16, height = 16, unit="cm", device = cairo_pdf)
+ggsave(plot=plot_feed_centered,"APPENDIX/Milchleistung/feed_samples.png", width = 16, height = 9, unit="cm")
+
